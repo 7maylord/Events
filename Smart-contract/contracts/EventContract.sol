3e88f58c961baa459484b0fc0830d38021610b6a
Olumide Adenigba <olumideadenigba@gmail.com>
2025-02-11 12:29:26 +0100

    chore: code cleanup

--- Smart-contract/contracts/EventContract.sol
+++ Smart-contract/contracts/EventContract.sol
@@ -9,14 +9,12 @@ contract EventContract {
         paid
     }
 
-    event EventCreated (uint256 indexed _id, address indexed _organizer);
+    event EventCreated(uint256 indexed _id, address indexed _organizer);
     event TicketPurchased(uint256 indexed _eventId, address indexed _buyer);
-    event TicketMinted(uint256 indexed _eventId, address _recipient, uint256 _tokenId);
+    event TicketMinted(uint256 indexed _eventId, address indexed_recipient, uint256 _tokenId);
     event AttendanceVerified(uint256 indexed eventId, address indexed attendee);
 
-
-
-      struct EventDetails {
+    struct EventDetails {
         string _title;
         string _description;
         uint256 _startDate;
@@ -31,7 +29,7 @@ contract EventContract {
     }
 
     uint256 public event_count;
-    mapping(uint256 => EventDetails) public events;
+    mapping(uint256 => EventDetails) events;
     mapping(address => mapping(uint256 => bool)) hasRegistered;
 
     // write functions
@@ -53,6 +51,9 @@ contract EventContract {
         require(_startDate > block.timestamp, 'START DATE MUST BE IN FUTURE');
 
         require(_startDate < _endDate, 'END DATE MUST BE GREATER');
+
+        require(_type == EventType.free || _ticketPrice > 0, "PAID EVENTS MUST HAVE TICKET PRICE > 0");
+
 
         // Change??
         EventDetails memory _updatedEvent = EventDetails ({
@@ -77,139 +78,73 @@ contract EventContract {
     }
 
     // register for an event
-    function registerForEvent(uint256 _event_id) external payable {
+    function registerForEvent(uint256 _eventId) external payable {
 
         require(msg.sender != address(0), 'INVALID ADDRESS');
 
-        require(_event_id <= event_count && _event_id != 0, 'EVENT DOES NoT EXIST');
+        require(_eventId <= event_count && _eventId != 0, 'No Event Exist');
         
         // get event details
-        EventDetails memory _eventInstance = events[_event_id];
+        EventDetails memory _eventInstance = events[_eventId];
 
         require(_eventInstance._endDate > block.timestamp, 'EVENT HAS ENDED');
 
         require(_eventInstance._registeredGuestCount < _eventInstance._expectedGuestCount, 'REGISTRATION CLOSED');
 
-        require(!hasRegistered[msg.sender][_event_id], 'ALREADY REGISTERED');
+        require(!hasRegistered[msg.sender][_eventId], 'ALREADY REGISTERED');
 
         if (_eventInstance._type == EventType.paid) {
-            //call internal func. for ticket purchase
-            purchaseTicket(_event_id);
-        }
+            require(msg.value >= _eventInstance._ticketPrice, "INSUFFICIENT FUNDS");
+
+            payable(_eventInstance._organizer).transfer(msg.value);
+            _eventInstance._registeredGuestCount++;
+            hasRegistered[msg.sender][_eventId] = true;
+        }     
+    
         else {
             // update registered event guest count
             _eventInstance._registeredGuestCount++;
             
             // updated has reg struct
-            hasRegistered[msg.sender][_event_id] = true;
-
-            // mint ticket to user
-            mint(_event_id, msg.sender);
-
+            hasRegistered[msg.sender][_eventId] = true;
         }
-    } 
-
-       // Purchase a ticket for a paid event
-    function purchaseTicket(uint256 _eventId) internal {
-        EventDetails memory _eventInstance = events[_eventId];
-
-        require(_eventInstance._type == EventType.paid, "EVENT IS NOT PAID");
-        require(_eventInstance._ticketPrice > 0, "INVALID TICKET PRICE");
-        require(msg.value >= _eventInstance._ticketPrice, "INSUFFICIENT FUNDS");
-
-        payable(_eventInstance._organizer).transfer(msg.value); // Transfer payment to organizer
-
-        _eventInstance._registeredGuestCount++;
-        hasRegistered[msg.sender][_eventId] = true;
-
+        // updated has events mapping
+        events[_eventId] = _eventInstance;
         emit TicketPurchased(_eventId, msg.sender);
         // Mint NFT ticket to msg.sender
-        // EventTicket(_eventInstance._ticketAddress).mint(msg.sender);
-        mint(_eventId, msg.sender);
-    }
-
+        EventTicket(_eventInstance._ticketAddress).mint(msg.sender, _eventId);
+       
+    } 
 
     function createEventTicket (uint256 _eventId, string memory _ticketname, string memory _ticketSymbol) external {
 
-        require(_eventId <= event_count && _eventId != 0, "EVENT DOESN'T EXIST");
+        require(_eventId <= event_count && _eventId != 0, "No Event Exist");
         
         EventDetails memory _eventInstance = events[_eventId];
 
-        require(msg.sender == _eventInstance._organizer, 'ONLY ORGANIZER CAN CREATE');
+        require(msg.sender == _eventInstance._organizer, 'ONLY ORGANIZER');
 
-        require(_eventInstance._ticketAddress == address(0), 'TICKET ALREADY CREATED');
+        require(_eventInstance._ticketAddress == address(0), 'ALREADY CREATED');
 
         EventTicket newTicket = new EventTicket(address(this), _ticketname, _ticketSymbol);
 
-        events[_eventId]._ticketAddress = address(newTicket);
+        _eventInstance._ticketAddress = address(newTicket);
 
-        // _eventInstance._ticketAddress = address(newTicket);
+        events[_eventId] = _eventInstance;
 
-    }
-
-    function mint(uint256 _eventId, address _recipient) internal {
-        EventDetails memory _eventInstance = events[_eventId];
-        require(_eventInstance._ticketAddress != address(0), "TICKET CONTRACT NOT CREATED");
-
-        EventTicket(_eventInstance._ticketAddress).mint(_recipient);
-        emit TicketMinted(_eventId, _recipient, _eventInstance._registeredGuestCount);
-   
-    }
+    }   
 
     // confirm/validate of tickets
     function validateTicket(uint256 _eventId, address _attendee) external {
-        require(_eventId > 0 && _eventId <= event_count, "EVENT DOESN'T EXIST");
-
+        require(_eventId > 0 && _eventId <= event_count, "No Event");
         EventDetails memory _eventInstance = events[_eventId];
-        require(_eventInstance._organizer == msg.sender, 'ONLY ORGANIZER CAN VALIDATE');
-        require(_eventInstance._ticketAddress != address(0), 'TICKET CONTRACT NOT CREATED');
-        require(EventTicket(_eventInstance._ticketAddress).ownerOf(_tokenId) != address(0), 'INVALID TICKET');
-        EventTicket(_eventInstance._ticketAddress).validateTicket(_tokenId);
-        _eventInstance._verifiedGuestCount++;
+        require(_eventInstance._organizer == msg.sender, "ONLY ORGANIZER CAN VALIDATE");
 
+        uint256 tokenId = EventTicket(_eventInstance._ticketAddress).getTokenForEvent(_attendee, _eventId);
+        EventTicket(_eventInstance._ticketAddress).validateTicket(tokenId, _eventId);
+
+        _eventInstance._verifiedGuestCount++;
+        events[_eventId] = _eventInstance;
         emit AttendanceVerified(_eventId, _attendee);
     }
-
-
-    // read functions
-
-    // function getEventDetails(uint256 _eventId) external view returns (EventDetails memory) {
-    //     return events[_eventId];
-    // }
-
-    function getEventDetails(uint256 _eventId) external view returns (
-    string memory title,
-    string memory description,
-    uint256 startDate,
-    uint256 endDate,
-    EventType eventType,
-    uint32 expectedGuestCount,
-    uint32 registeredGuestCount,
-    uint32 verifiedGuestCount,
-    address organizer,
-    address ticketContract,
-    uint256 ticketPrice
-) {
-    require(_eventId <= event_count && _eventId != 0, "EVENT DOESN'T EXIST");
-
-    EventDetails memory eventInstance = events[_eventId];
-
-    return (
-        eventInstance._title,
-        eventInstance._description,
-        eventInstance._startDate,
-        eventInstance._endDate,
-        eventInstance._type,
-        eventInstance._expectedGuestCount,
-        eventInstance._registeredGuestCount,
-        eventInstance._verifiedGuestCount,
-        eventInstance._organizer,
-        eventInstance._ticketAddress,
-        eventInstance.ticketPrice
-    );
-}
-    function isRegisteredForEvent(address _attendee, uint256 _eventId) external view returns (bool) {
-    require(_eventId <= event_count && _eventId != 0, "EVENT DOESN'T EXIST");
-    return hasRegistered[_attendee][_eventId];
-}
 }
--- Smart-contract/contracts/EventTicket.sol
+++ Smart-contract/contracts/EventTicket.sol
@@ -2,24 +2,59 @@
 pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title EventTicket
+ * @dev ERC721 contract for event ticketing, where each ticket is uniquely linked to an event.
+ */
+contract EventTicket is ERC721Enumerable, Ownable {
+    uint256 private _ticketCounter; // Counter to track ticket IDs
+    address public eventContract; // Address of the event contract
+    mapping(uint256 => uint256) public ticketEventMapping; // Maps token ID to event ID
 
-contract EventTicket is ERC721Enumerable, Ownable(msg.sender) {
-    uint256 private _ticketCounter;
-    address public eventContract;
-
+    
     constructor(address _eventContract, string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
+        Ownable(msg.sender)
     {
         eventContract = _eventContract;
     }
 
-    function mint(address _recipient) external {
+    
+    function mint(address _recipient, uint256 _eventId) external {
         require(msg.sender == eventContract, "Only event contract can mint tickets");
-        ++_ticketCounter;
+        _ticketCounter++;
         _safeMint(_recipient, _ticketCounter);
+        ticketEventMapping[_ticketCounter] = _eventId;
+    }
+
+    function getTokensByAddress(address _owner) external view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(_owner);
+        require(tokenCount > 0, "No tickets owned by this address");
+
+        uint256[] memory tokenIds = new uint256[](tokenCount);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function getTokenForEvent(address _owner, uint256 _eventId) external view returns (uint256) {
+        uint256 tokenCount = balanceOf(_owner);
+        require(tokenCount > 0, "No tickets owned by this address");
+
+        for (uint256 i = 0; i < tokenCount; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(_owner, i);
+            if (ticketEventMapping[tokenId] == _eventId) {
+                return tokenId;
+            }
+        }
+        revert("No ticket found for this event");
+    }
+
+    function validateTicket(uint256 _tokenId, uint256 _eventId) external {
+        require(ownerOf(_tokenId) != address(0), "Invalid ticket");
+        require(ticketEventMapping[_tokenId] == _eventId, "Ticket does not match event");
     }
 }
